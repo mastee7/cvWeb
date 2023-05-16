bb0d6e954b0362629849218ca175abd54cbe3ad6
woojeh <woojeh7@gmail.com>
2023-05-15 21:17:43 -0700

    links added

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function About() {
   return (
-    <h1>About</h1>
+    <div>
+      <span className="about_club">
+        Hi!
+        We are a group of highly motivated, passionate, and curios students from different backgrounds.
+        This club provides a platform for students to connect, learn, and start or work on projects 
+        particularly in the computer vision area as one of the applications for machine learning.
+      </span>
+    </div>
   );
 }
