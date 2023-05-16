d10cb8906491de9dbec2ec0ea49b9404c0403f26
woojeh <woojeh7@gmail.com>
2023-05-15 20:16:09 -0700

    Route works

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import cvlogo from './assets/cv_logo.png'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from './components/about/about'
+import Home from './components/home/home'
+import Projects from './components/projects/projects'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={cvlogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={cvlogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={< Home />}/>
+        <Route path="/about" element={< About />}/>
+        <Route path="/projects" element={< Projects />}/>
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
---
+++ src/components/home/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import cv_logo from '../../assets/cv_logo.png'
+
+export default function Home() {
+  return (
+    <>
+        <img src={cv_logo} alt="cv_logo" />
+        <h1>home</h1>
+    </>
+  );
+}
